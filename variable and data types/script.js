de235e32a7e3e2daab7b117cd197ed9c953d90be
Riddhi Bose <riddhi.25b03110039@abes.ac.in>
2026-02-25 09:51:16 +0530

    js

---
+++ variable and data types/script.js
@@ -0,0 +1,25 @@
+/*Primitive Data types 
+Number: 1,2,3 etc
+string:characters enclosed in quotes
+Boolean: true or false
+Undefined
+Null
+
+undefined ==null(true) but undefinedd ===null(false)
+== :- value is same ex- x= 10, x= "10".... it is equal in value
+=== :- type not same ex- x= 10, x= "10"... data types not same*/
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = false; 
+console.log(num);
+console.log(typeof(num));
+num = "name";
+console.log(num);
+console.log(typeof(num));
+num = undefined;
+console.log(num);
+console.log(typeof(num));
+num= null;
+console.log(num);
+console.log(typeof(num));
