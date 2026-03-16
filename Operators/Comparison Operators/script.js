5865c4dadf610da61ec94feb5ef338db39d7dbb2
Riddhi Bose <riddhi.25b03110039@abes.ac.in>
2026-03-16 22:09:02 +0530

    new

---
+++ Operators/Comparison Operators/script.js
@@ -0,0 +1,43 @@
+/*ogical Operators
+&& (AND) - true if both operands are true
+|| (OR) - true if either operand is true
+! (NOT) - true if operand is false
+
+Ternary Operator
+condition ? expression1 : expression2
+
+question find the maximum of three numbers using ternary operator
+*/
+var a =10;
+var b =20;
+var linebreak= "<br>";
+
+document.write("a==b) => ");
+result = (a==b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<b) => ");
+result= (a<b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>b) => ");
+result = (a>b);
+document.write(result);
+document.write(linebreak);
+
+document.write("a!=b) => ");
+result = (a!=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a<=b) => ");
+result = (a<=b);
+document.write(result);
+document.write(linebreak);
+
+document.write("(a>=b) => ");
+result = (a>=b);
+document.write(result);
+document.write(linebreak);
